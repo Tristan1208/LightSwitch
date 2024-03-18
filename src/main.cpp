bbe7d71b4c052adf748579c8e3a8b7ccfe413758
Tristan1208 <dupt2901@usherbrooke.ca>
2024-03-17 20:30:11 -0400

    motor library

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Servo servo;  // create servo object to control a servo
+int servoPin = 13; // GPIO pin for servo control
+int pos =0;    // variable to store the servo position
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	servo.setPeriodHertz(50);    // standard 50 hz servo
+	servo.attach(servoPin, 500, 2400);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+		servo.write(180);    
+		delay(2000);
+		servo.write(0);
+		delay(2000);             
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
